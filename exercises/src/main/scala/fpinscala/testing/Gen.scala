b357b5a009d4d4910e8c626ca6feacc42349a492
Chao Chu <chuchao333@gmail.com>
2015-10-12 07:41:02 +0800

    Finish 8.13 and 8.14 and finish reading chapter 8

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -22,26 +22,26 @@ shell, which you can fill in and modify while working through the chapter.
 //  }
 //}
 
-case class Prop(run: (TestCases, RNG) => Result) {
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
 
   // exercise 8.9
   def &&(p: Prop): Prop = Prop {
-    (n, rng) => run(n, rng) match {
-      case Passed => p.run(n, rng)
+    (max, n, rng) => run(max, n, rng) match {
+      case Passed | Proved => p.run(max, n, rng)
       case f: Falsified => f
     }
   }
 
   def ||(p: Prop): Prop = Prop {
-    (n, rng) => run(n, rng) match {
-      case Falsified(failedMsg, count) => p.tag(failedMsg).run(n, rng)
-      case Passed => Passed
+    (max, n, rng) => run(max, n, rng) match {
+      case Falsified(failedMsg, count) => p.tag(failedMsg).run(max, n, rng)
+      case p @ (Proved | Passed) => p
     }
   }
 
   // Tag an extra error message (by prepending) to the original error message in case of failure
   private def tag(errorMsg: String): Prop = Prop {
-    (n, rng) => run(n, rng) match {
+    (max, n, rng) => run(max, n, rng) match {
       case Falsified(failedMsg, count) => Falsified(errorMsg + "\n" + failedMsg, count)
       case Passed => Passed
     }
@@ -53,6 +53,7 @@ object Prop {
   type FailedCase = String
   type SuccessCount = Int
   type TestCases = Int
+  type MaxSize = Int
   // type Result = Option[(FailedCase, SuccessCount)]
 
   sealed trait Result {
@@ -67,8 +68,12 @@ object Prop {
     def isFalsified = true
   }
 
+  case object Proved extends Result {
+    def isFalsified = false
+  }
+
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = Prop {
-    (n: TestCases, rng: RNG) => {
+    (max: MaxSize, n: TestCases, rng: RNG) => {
       randomStream(gen)(rng).zip(Stream.from(0)).take(n).map {
         case (a, i) => try {
           if (f(a)) Passed else Falsified(a.toString, i)
@@ -79,16 +84,58 @@ object Prop {
     }
   }
 
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max: MaxSize, n: TestCases, rng: RNG) => {
+      // for each size, generate this many random cases.
+      val casesPerSize = (n + (max - 1)) / max
+      val props: Stream[Prop] =
+      // Make one property per size, but no more than n properties.
+        Stream.from(0).take((n min max) + 1) map { i =>
+          forAll(g(i))(f)
+        }
+
+      val prop: Prop = props.map(p => Prop { (max, _, rng) =>
+        p.run(max, casesPerSize, rng)
+      }).toList.reduce(_ && _)
+
+      prop.run(max, n, rng)
+    }
+  }
+
+  def forAll[A](sgen: SGen[A])(f: A => Boolean): Prop =
+    forAll(n => sgen.g(n))(f)
+
+  def run(prop: Prop, maxSize: MaxSize = 100, testCases: TestCases = 100,
+           rng: RNG = RNG.Simple(System.currentTimeMillis)): Unit =
+    prop.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests: \n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Proved =>
+        println(s"+ OK, proved property.")
+    }
+
+  def badCheckImpl(p: => Boolean): Prop = {
+    // result is memorized to avoid re-computation
+    lazy val result = p
+    forAll(Gen.unit(()))(_ => result)
+  }
+
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Proved else Falsified("()", 0)
+  }
+
   private def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
     Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
 
   private def buildMsg[A](s: A, e: Exception): String =
     s"test case: $s\n" +
-    s"generated an exception: ${e.getMessage}\n" +
-    s"stack trace: \n ${e.getStackTrace.mkString("\n")}"
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace: \n ${e.getStackTrace.mkString("\n")}"
 }
 
-case class Gen[A](sample: State[RNG, A]) {
+case class Gen[+A](sample: State[RNG, A]) {
   // exercise 8.6
   def map[B](f: A => B): Gen[B] = Gen(sample map f)
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
@@ -97,7 +144,6 @@ case class Gen[A](sample: State[RNG, A]) {
   // exercise 8.10
   def unsized: SGen[A] = SGen(_ => this)
 }
-
 
 object Gen {
   // exercise 8.4
@@ -124,11 +170,15 @@ object Gen {
   // exercise 8.12
   def listOf[A](g: Gen[A]): SGen[List[A]] =
     SGen(n => listOfN(n, g))
+
+  // exercise 8.13
+  def nonEmptyListOf[A](g: Gen[A]): SGen[List[A]] =
+    SGen(n => listOfN(n max 1, g))
 }
 
 // We want SGen[A] could be used wherever a Gen[A] is expected, thus covariant on A
 case class SGen[+A](g: Int => Gen[A]) {
-  // def apply(n: Int) = g(n)
+  def apply(n: Int) = g(n)
 
   // exercise 8.11
   def map[B](f: A => B): SGen[B] =
@@ -138,16 +188,34 @@ case class SGen[+A](g: Int => Gen[A]) {
     SGen(g andThen (_ flatMap f))
 }
 
-//object Gen {
-//  def unit[A](a: => A): Gen[A] = ???
-//}
-//
-//trait Gen[A] {
-//  def map[A,B](f: A => B): Gen[B] = ???
-//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-//}
-//
-//trait SGen[+A] {
-//
-//}
+object Example extends App {
+  val smallInt = Gen.choose(-10, 10)
+
+//  Prop.run(Prop.forAll(Gen.listOf(smallInt)) { ns =>
+//    // List.max will throw on empty list
+//    val max = ns.max
+//    !ns.exists(_ > max)
+//  })
+
+  Prop.run(Prop.forAll(Gen.nonEmptyListOf(smallInt)) { ns =>
+    val max = ns.max
+    !ns.exists(_ > max)
+  })
+
+  // exercise 8.14
+  val sortedProp = forAll(Gen.listOf(smallInt)) { ns =>
+    (ns.isEmpty ||
+      (ns.sorted.foldLeft((true, Int.MinValue)) { case ((flag, cur), next) =>
+        (flag && (cur <= next), next)
+      })._1) &&
+      ns.forall(ns.sorted.contains(_)) &&
+      ns.sorted.forall(ns.contains(_))
+  }
+  Prop.run(sortedProp)
+
+  // although the result is memorized inside 'check', the test runner will still generate multiple test cases
+  // (due to the use of 'forAll')
+  // Prop.run(Prop.badCheckImpl(true))
+  Prop.run(Prop.check(true))
+}
 
